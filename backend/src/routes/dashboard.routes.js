fb135e0065f065e082acaba9214e8a285ab8693c
codehunt022 <arunnath055@gmail.com>
2026-04-04 17:56:11 +0530

    prettier

--- backend/src/routes/dashboard.routes.js
+++ backend/src/routes/dashboard.routes.js
@@ -7,22 +7,10 @@ import auth from "../middleware/auth.js";
 import { requireMinRole } from "../middleware/rbac.js";
 
 router.use(auth);
-
-
 router.get("/summary", requireMinRole("viewer"), getSummary);
-
-
 router.get("/category-breakdown", requireMinRole("viewer"), getCategoryBreakdown);
-
-
-
-
 router.get("/monthly-trends", requireMinRole("analyst"), getMonthlyTrends);
-
-
 router.get("/weekly-trends", requireMinRole("analyst"), getWeeklyTrends);
-
-
 router.get("/insights", requireMinRole("analyst"), getInsights);
 
 export default router;
--- backend/src/routes/record.routes.js
+++ backend/src/routes/record.routes.js
@@ -2,27 +2,15 @@ import express from "express";
 const router = express.Router();
 
 import {getAllRecords, getRecordById, createRecord, updateRecord, deleteRecord} from "../controllers/record.controller.js";
-
 import auth from "../middleware/auth.js";
 import { requireRole, requireMinRole } from "../middleware/roleAccess.js";
-
 import {createRecordValidator, updateRecordValidator, filterRecordValidator} from "../validations/recordValidation.js";
 
-
 router.use(auth);
-
-
 router.get("/", requireMinRole("viewer"), filterRecordValidator, getAllRecords);
-
-
 router.get("/:id", requireMinRole("viewer"), getRecordById);
-
-
 router.post("/", requireRole("admin"), createRecordValidator, createRecord);
-
 router.patch("/:id", requireRole("admin"), updateRecordValidator, updateRecord);
-
-
 router.delete("/:id", requireRole("admin"), deleteRecord);
 
 export default router;
